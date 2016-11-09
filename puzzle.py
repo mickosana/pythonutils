7cb66b28fc76b45a30918b211890b6d09e637875
micthaworm <ckprofmyk@gmail.com>
2016-11-09 14:40:28 +0200

    took me 5 minutes

---
+++ puzzle.py
@@ -0,0 +1,20 @@
+
+word='g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+bd=list(word)
+for char in bd:
+    idx=bd.index(char)
+    if char == 'k':
+        bd[idx]= 'm'
+    elif char =='o':
+        bd[idx]='q'
+    elif char =='e':
+        bd[idx]='g'
+    elif char=='m':
+        bd[idx]='k'
+    elif char== 'q':
+        bd[idx]='o'
+    elif char=='g':
+        bd[idx]='e'
+print(bd)
+    
+    
